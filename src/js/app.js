02e73346dc97eb6020588f11383863ce15e5c8ee
mihvlad08 <mihvlad08@gmail.com>
2021-01-24 17:29:27 +0200

    added animated textarea on button click

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
-function redirect(arg) {
-    
-}
 
-function dropwdown() {
-    
+function dropdown() {
+    var element = document.getElementById("w3review");
+    element.style.visibility = "visible";
+    element.classList.add("animate__animated");    
+    element.classList.add("animate__fadeIn");
 }
